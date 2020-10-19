4e536e5b4f592a3ca6c91246fb7352e9cd5111d9
tony-developer <anton.serebriackoff@yandex.ru>
2020-10-19 19:16:16 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./style.css"
+
+let someStr = 'Изменил, сохранил, собрал'
+console.log('Hello, Webpack2222333')
+
+const numbers = [2, 3, 5];
+const doubledNumbers = numbers.map(number => number * 2); // Стрелочная функция. Не запнётся ли на ней Internet Explorer
+
+console.log(doubledNumbers);
